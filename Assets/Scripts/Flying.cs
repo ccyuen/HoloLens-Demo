288d2383ce488aa5698001c1a82950c01ac5a75f
Clement Yuen <clementchaoyuen@gmail.com>
2017-02-03 09:43:06 -0500

    Added Unity project to GitHub

---
+++ Assets/Scripts/Flying.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour {
+
+    bool selected = false;
+    public GameObject ship;
+    private Rigidbody shiprb;
+    public ParticleSystem leftBooster;
+    public ParticleSystem rightBooster;
+
+    void Start()
+    {
+        shiprb = ship.GetComponent<Rigidbody>();
+        leftBooster.Stop();
+        rightBooster.Play();
+    }
+
+    void OnSelect()
+    {
+        // On each Select gesture, toggle whether the user is in placing mode.
+        selected = !selected;
+
+        // If the user has selected the ship, turn off gravity and turn on boosters for lift off
+        if (selected)
+        {
+            leftBooster.Play();
+            rightBooster.Play();
+            shiprb.useGravity = false;
+            shiprb.isKinematic = true;
+        }
+        // If the user has not selected the ship, keep gravity on and boosters turned off
+        else
+        {
+            leftBooster.Stop();
+            rightBooster.Stop();
+            shiprb.useGravity = true;
+            shiprb.isKinematic = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SpeechHandler.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows.Speech;
+
+public class SpeechHandler : MonoBehaviour
+{
+    public string HidePlaneCmd = "hide plane";
+    public GameObject Plane;
+
+    public string ShootCmd = "shoot";
+    public CannonBehavior Cannon;
+
+    public string ResetSceneCmd = "reset scene";
+    public string ChangeShootCmd = "enable";
+    public string SpawnCmd = "spawn";
+    public string DestroyCmd = "destroy";
+    private bool isGazing = false;
+
+    private KeywordRecognizer _keywordRecognizer;
+
+    void Start()
+    {
+        _keywordRecognizer = new KeywordRecognizer(new[] { HidePlaneCmd, ResetSceneCmd, ShootCmd, ChangeShootCmd });
+        _keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        _keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        var cmd = args.text;
+        if (cmd == HidePlaneCmd)
+        {
+            Plane.SetActive(false);
+        }
+        else if (cmd == ShootCmd)
+        {
+            Cannon.Shoot();
+        }
+        else if (cmd == ResetSceneCmd)
+        {
+            SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+        }
+        else if (cmd == ChangeShootCmd)
+        {
+            Cannon.enableShoot();
+        }/*
+        else if (cmd == SpawnCmd)
+        {
+            spawn();   
+        }
+        else if (cmd == DestroyCmd)
+        {
+            destroy();
+        }*/
+    }
+
+    /*
+    // create more holograms that is being gazed at
+    void spawn()
+    {
+        if (FocusedObject != null)
+            GameObject newObject = (GameObject)Instantiate(Resources.Load("bulbasaur"));
+    }
+
+    // destroy hologram that is being gazed at
+    void destroy()
+    {
+
+    }*/
+   
+
+    private void OnDestroy()
+    {
+        if (_keywordRecognizer != null)
+        {
+            if (_keywordRecognizer.IsRunning)
+            {
+                _keywordRecognizer.Stop();
+            }
+            _keywordRecognizer.Dispose();
+        }
+    }
+}
